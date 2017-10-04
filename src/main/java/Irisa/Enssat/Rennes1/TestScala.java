647166559364f774cecf801ddbca352448d86f0b
dungltr <dung80@gmail.com>
2017-10-04 10:28:40 +0200

    aa

---
+++ src/main/java/Irisa/Enssat/Rennes1/TestScala.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Irisa.Enssat.Rennes1;
+
+//import Scala.SimpleApp;
+import Scala.SecondScala;
+import Scala.ThirdScala;
+
+/**
+ *
+ * @author letrung
+ */
+public class TestScala {
+    public static void test(){
+//        SimpleApp ScalaApp = new SimpleApp();
+//        ScalaApp.main();
+//        SecondScala Second = new SecondScala();
+//        Second.main_test();
+        ThirdScala Third = new ThirdScala();
+        Third.main_third();
+    }
+    
+}
