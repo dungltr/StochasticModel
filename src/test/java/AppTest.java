9240c3180d36d701979ea41dc4b1ebba15360d78
dungltr <dung80@gmail.com>
2017-10-17 16:34:09 +0200

    Create Move Hive Postgres

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -160,7 +160,8 @@ public class AppTest{
 //        LinearRegressionManual.test_minDataset(training_file,testing_file,0.8);
 //        LinearRegressionManual.TPCH(0.0, "", "100m", "Hive", "Hive", "predict");
 //        Irisa.Enssat.Rennes1.TestScript.testall();
-	TPCHQuery.TPCH(Math.random(),"tpch","100m","Hive","Postgres","training");
+//	TPCHQuery.TPCH(Math.random(),"tpch","100m","Hive","Postgres","training");
+        TPCHQuery.Move(Math.random(),"tpch","100m","Hive","Postgres","Move");
     }
 //    @Test
     public void testall() throws Exception{
