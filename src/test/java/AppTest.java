65f85fe723bd13e2b2aeda9516d95a2de8b8ab8a
dungltr <dung80@gmail.com>
2017-10-30 16:39:25 +0100

    add Join

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -163,7 +163,7 @@ public class AppTest{
 //        Irisa.Enssat.Rennes1.TestScript.testall();
 	for (int i =0; i<1; i++)
         { 
-//          TPCHQuery.Move(Math.random(),"tpch","100m","Hive","Postgres","Move");
+          TPCHQuery.Move(Math.random(),"tpch","100m","Hive","Postgres","Move");
 //          TPCHQuery.Move(Math.random(),"tpch","100m","Postgres","Hive","Move");
 //          TPCHQuery.Move(Math.random(),"tpch","100m","Hive","Hive","Move");
 //          TPCHQuery.Move(Math.random(),"tpch","100m","Postgres","Postgres","Move");
@@ -171,7 +171,7 @@ public class AppTest{
 //          TPCHQuery.Join(Math.random(),"tpch","100m","Hive","Hive","Join");
 //          TPCHQuery.WorkflowMove(Math.random(),"tpch","100m","Hive","Postgres","Move");
 //        TPCHQuery.WorkflowJoin(Math.random(),"tpch","100m","Hive","Hive","Join");
-        TPCHQuery.WorkflowJoinMove(Math.random(),"tpch","100m","Hive","Postgres","Join");
+//        TPCHQuery.WorkflowJoinMove(Math.random(),"tpch","100m","Hive","Postgres","Join");
 //        IRES.TestWorkFlow.createWorkflowJoin();
 //	IRES.TestWorkFlow.smallworkflow();
 //        IRES.TestWorkFlow.workflow();
