457747341661dbc71bf6e8de03b241b3d97fd9d4
dungltr <dung80@gmail.com>
2017-10-31 10:28:03 +0100

    add Join

--- src/main/java/IRES/TPCHQuery.java
+++ src/main/java/IRES/TPCHQuery.java
@@ -880,6 +880,7 @@ public class TPCHQuery {
         size[2] = R2;
         return size;
         }
-         
+        double[] size = new double [numberOfSize];
+    return size;     
     }   
 }
