0c0b202a7fadb9135793d85159cf3afe60ea1f0c
dungltr <dung80@gmail.com>
2018-04-18 18:02:07 +0200

    test

--- src/main/java/Algorithms/Algorithms.java
+++ src/main/java/Algorithms/Algorithms.java
@@ -581,7 +581,7 @@ public class Algorithms {
         else return SQL.substring(SQL.lastIndexOf("DROP TABLE IF EXISTS "),SQL.indexOf("_"))
                 .replaceAll("DROP TABLE IF EXISTS ","")
                 .replaceAll(" ","");
-        
+
     }
     public static void mainIRES(Move_Data Data, String SQL, YarnValue yarnValue, double TimeOfDay, double[] size, String KindOfRunning ) throws Exception{      
         String OperatorFolder = operatorFolder();
