75186162b5f054915f84b3606c04eb2a5316661c
dungltr <dung80@gmail.com>
2018-05-11 14:46:58 +0200

    Fix Test TPCH Join

--- src/main/java/IRES/testQueryPlan.java
+++ src/main/java/IRES/testQueryPlan.java
@@ -243,7 +243,8 @@ public class testQueryPlan {
                         //, "clinicaltrial_text"//good
                         //, "filemetaelement_text"
                         "sequenceattributes_text"
-                        ,"generalinfotable_text"
+                        ,"generalinfotable_texta"
+                        ,"generalinfotable_textb"
                         };
                         //
 
